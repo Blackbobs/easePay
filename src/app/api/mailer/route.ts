c432fe73f58735b152a9c918beaf516d12f7e799
Blackbobs <ayodejiayodele350@gmail.com>
2025-10-02 05:28:50 +0100

    fix:fixed th smtp issue

--- src/app/api/mailer/route.ts
+++ src/app/api/mailer/route.ts
@@ -14,6 +14,8 @@ export async function POST(req: Request) {
 
     const transporter = nodemailer.createTransport({
       service: "gmail", // or SMTP details
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, // or App Password if using Gmail
